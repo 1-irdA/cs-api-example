9740a143bb0b81bb2ca167d117f2e85b4184d710
GARROUSTE Adrien <garrousteadrien@gmail.com>
2022-02-25 08:48:48 +0100

    Add health check

--- Program.cs
+++ Program.cs
@@ -1,14 +1,20 @@
 using CsApiExample.Models;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+string connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING") ?? builder.Configuration.GetConnectionString("Connection");
+
 builder.Services.AddControllers();
-builder.Services.AddDbContext<CustomerContext>(opt => {
-    opt.UseSqlServer(Environment.GetEnvironmentVariable("CONNECTION_STRING") ?? builder.Configuration.GetConnectionString("Connection"));
-});
+builder.Services.AddDbContext<CustomerContext>(opt => opt.UseSqlServer(connectionString));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHealthChecks()
+                .AddSqlServer(
+                    connectionString, 
+                    healthQuery: "SELECT 1", 
+                    tags: new[] { "ready" });
 
 var app = builder.Build();
 
@@ -21,6 +27,21 @@ if (app.Environment.IsDevelopment())
 
 app.UseAuthorization();
 
-app.MapControllers();
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+
+    endpoints.MapHealthChecks("/health/live", new HealthCheckOptions
+    {
+        Predicate = (_) => false
+    });
+
+    endpoints.MapHealthChecks("/health/ready", new HealthCheckOptions
+    {   
+        Predicate = (check) => check.Tags.Contains("ready")
+    });
+});
 
 app.Run();
